70a2b3dc61907e1e044bbbb2f30ac563b48634b7
jayesh-rao <jayeshr007@gmail.com>
2018-02-25 20:06:33 +0530

    added few more algos

--- lambda.cc
+++ lambda.cc
@@ -6,6 +6,7 @@
 
 using namespace std;
 
+
 template<typename T>
 class MyRange {
 public:
--- reverse.cc
+++ reverse.cc
@@ -1,18 +1,39 @@
-#include<iostream>
+/*
+* problem:
+* input => a string of words
+* o/p => reveresed words
+* ex: hello world ==> world hello
+*/
+
+#include <iostream>
 #include <string>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
-string
+void
 reverseWords(const string& s) {
     string rev(s.rbegin(), s.rend());
-    return rev;
+
+    istringstream iss(rev);
+    vector<string> words{istream_iterator<string>(iss), istream_iterator<string>()};
+    int c = words.size() - 1;
+
+    for (auto &word : words) {
+    	cout << string(word.rbegin(), word.rend());
+    	if (c-- > 0) cout << " ";
+
+    }
+
+    //return s;
 }
 
 
 
 int main() {
     string s = "sky is blue today";
-    cout << endl << reverseWords(s);
+    reverseWords(s);
+    //cout << endl << reverseWords(s);
     cout << endl;
 }
